a10f31985d17832f82065fcd84f090a5d3809a5c
Polluxs <thomasdorissen@gmail.com>
2025-11-06 09:39:07 +0100

    add existing user table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,13 @@
-import { pgTable, serial, integer } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp, boolean, bigserial, } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+	id: bigserial('id', { mode: 'number' }).primaryKey(),
+	email: text('email').notNull().unique(),
+	emailVerified: boolean('email_verified').default(false),
+	// TODO: Make these non-nullable once registration flow is updated
+	// For now nullable to test infrastructure
+	firstName: text('first_name'),
+	lastName: text('last_name'),
+	createdAt: timestamp('created_at', { withTimezone: true, mode: 'date' }).notNull().defaultNow(),
+	updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'date' }).notNull().defaultNow()
 });
